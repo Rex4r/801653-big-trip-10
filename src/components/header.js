a2fda52ca7cedc165b68e3c53d43939cdc679ba1
rex4r <1477893>
2019-12-13 00:56:56 +0700

    module2-task1

---
+++ src/components/header.js
@@ -0,0 +1,5 @@
+import {createTripInfoTemplate} from "./trip-info.js";
+import {createMenuTemplate} from "./menu.js";
+import {createFilterTemplate} from "./filter.js";
+
+export {createTripInfoTemplate, createMenuTemplate, createFilterTemplate};
